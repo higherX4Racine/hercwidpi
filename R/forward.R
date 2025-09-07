eb0204427548e169ff7c8eb30fe64c6f375c0898
Ben Taft <ben.taft@career2cradle.org>
2025-09-06 22:33:55 -0500

    tools and unit tests for loading and wrangling Forward Exam data

---
+++ R/forward.R
@@ -0,0 +1,90 @@
+## Copyright (C) 2025 by Higher Expectations for Racine County
+
+#' Helper predicate for detecting when students took the Forward exam.
+#'
+#' @param .result_codes *&lt;chr&gt;* usually the TEST_RESULT_CODE column from a WISEDash data set.
+#'
+#' @returns *&lt;lgl&gt;* `TRUE` when the `.result_codes` value contains a digit
+#' @export
+is_tested <- function(.result_codes) {
+    stringr::str_detect(.result_codes, "\\D", negate = TRUE)
+}
+
+#' Helper predicate for detecting when students passed the Forward exam.
+#'
+#' @param .result_codes *&lt;chr&gt;* usually the TEST_RESULT_CODE column from a WISEDash data set.
+#' @param .proficiency_threshold *&lt;int&gt;* the lowest result code that counts as success
+#'
+#' @returns *&lt;lgl&gt;* `TRUE` when the numeric value of `.result_codes` meets or exceeds the `.proficiency_threshold`
+#' @export
+is_proficient <- function(.result_codes, .proficiency_threshold = 3L) {
+    .tmp <- .result_codes |>
+        as.integer() |>
+        suppressWarnings()
+    !is.na(.tmp) & .tmp >= .proficiency_threshold
+}
+
+#' Helper for conditionally including values in a total
+#'
+#' @param .counts *&lt;int&gt;* counts, a la `STUDENT_COUNT`, that may be incorporated in a total
+#' @param .mask *&lt;lgl&gt;* whether or not a value of `.counts` should be included
+#'
+#' @returns `.counts` when `.mask` is true, or zero otherwise
+#'
+#' @keywords internal
+count_if <- function(.counts, .mask) {
+    dplyr::if_else(.mask, .counts, 0L)
+}
+
+#' Syntactic sugar for loading reports about the Forward Exam.
+#'
+#' @inheritParams read_wisedash_public
+#'
+#' @returns a data frame with 20 columns, as described by [SPEC_FOR_FORWARD]
+#' @export
+#' @seealso [read_wisedash_public()]
+#' @seealso [SPEC_FOR_FORWARD]
+load_forward <- function(.files) {
+    read_wisedash_public(.files, SPEC_FOR_FORWARD)
+}
+
+#' Summarize forward results
+#'
+#' @param .forward_data a data frame with columns from [`SPEC_FOR_FORWARD`]
+#' @param .proficiency_threshold *&lt;int?&gt;* the lowest score code that counts as proficient (defaults is `3`)
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> *&lt;chr&gt;* grouping fields for computing testing and success rates.
+#'
+#' @returns a data frame with `len(...) + 6 columns`
+#' \describe{
+#'   \item{...}{each grouping field}
+#'   \item{Students}{*&lt;int&gt;* the total number of students in the group}
+#'   \item{Score}{*&lt;dbl&gt;* the mean scale score of the group's students}
+#'   \item{Tested}{*&lt;int&gt;* the number of students in the group who took the test}
+#'   \item{Proficient}{*&lt;int&gt;* the number of students who were tested and achieved proficiency}
+#'   \item{Testing Rate}{*&lt;dbl&gt;* the proportion of students in the group who took the test}
+#'   \item{Proficiency Rate}{*&lt;dbl&gt;* the proportion of students who were tested and achieved proficiency}
+#' }
+#' @export
+wrangle_forward <- function(.forward_data, ..., .proficiency_threshold = 3L) {
+
+    .forward_data |>
+        dplyr::rename(
+            Students = "GROUP_COUNT",
+            Score = "FORWARD_AVERAGE_SCALE_SCORE"
+        ) |>
+        dplyr::mutate(
+            Tested = count_if(.data$STUDENT_COUNT,
+                              is_tested(.data$TEST_RESULT_CODE)),
+            Proficient = count_if(.data$STUDENT_COUNT,
+                                  is_proficient(.data$TEST_RESULT_CODE))
+        ) |>
+        dplyr::summarize(
+            dplyr::across(c("Students", "Score"),
+                          dplyr::first),
+            dplyr::across(c("Tested", "Proficient"),
+                          \(.)sum(., na.rm = TRUE)),
+            `Testing Rate` = .data$Tested / .data$Students,
+            `Success Rate` = .data$Proficient / .data$Tested,
+            .by = c(...)
+        )
+}
---
+++ tests/testthat/test-forward.R
@@ -0,0 +1,54 @@
+test_that("**is_tested** detects integers in a character vector", {
+  expect_equal(is_tested(c("foo", "3", "99", "-1", "12", NA)),
+               c(FALSE, TRUE, TRUE, FALSE, TRUE, NA))
+})
+
+test_that("**is_proficient** rates 3+ as a pass by default", {
+    expect_equal(is_proficient(c("foo", "3", "99", "-1", "12", NA)),
+                 c(FALSE, TRUE, TRUE, FALSE, TRUE, FALSE))
+})
+
+test_that("**is_proficient** can change its threshold", {
+    expect_equal(is_proficient(c("foo", "3", "99", "-1", "12", NA), 4),
+                 c(FALSE, FALSE, TRUE, FALSE, TRUE, FALSE))
+    expect_equal(is_proficient(c("foo", "3", "99", "-1", "12", NA), 13),
+                 c(FALSE, FALSE, TRUE, FALSE, FALSE, FALSE))
+    expect_equal(is_proficient(c("foo", "3", "99", "-1", "12", NA), 100),
+                 c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE))
+})
+
+test_that("**count_if** filters correctly", {
+    expect_equal(count_if(c(1, 1, 2, 3, 5, 8),
+                          c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE)),
+                 c(1, 0, 2, 0, 5, 0))
+    expect_equal(count_if(c(1, 1, 2, 3, 5, 8),
+                          c(FALSE, TRUE, FALSE, TRUE, FALSE, TRUE)),
+                 c(0, 1, 0, 3, 0, 8))
+})
+
+test_that("**load_forward** handles the example sheet", {
+    expect_snapshot_value(load_forward(test_path("data/forward_example.csv")),
+                          style = "serialize")
+})
+
+test_that("**wrangle_forward** makes the correct summaries", {
+    EXAMPLE_FORWARD <- readRDS(test_path("data/pretend forward data.rds"))
+    wrangled <- wrangle_forward(EXAMPLE_FORWARD,
+                                "DISTRICT_CODE",
+                                "SCHOOL_CODE",
+                                "GROUP_BY",
+                                "GROUP_BY_VALUE")
+    expect_equal(wrangled,
+                 tibble::tibble(
+                     DISTRICT_CODE = rep(c("0000", "0248", "1123"), c(3, 6, 3)),
+                     SCHOOL_CODE = rep(c(NA, "0120", "9100", NA), each = 3),
+                     GROUP_BY = rep("Disability Status", 12),
+                     GROUP_BY_VALUE = rep(c("SwD", "SwoD", "Unknown"), 4),
+                     Students = c(42, 29, 21, 37, 39, 28, 21, 39, 28, 34, 35, 26),
+                     Score = c(614.2895298, 1175.699427, 1154.949265, 382.6420502, 1554.754784, 1305.167718, 1390.459944, 350.2674766, 330.0761377, 2070.312619, 1313.815998, 1130.531237),
+                     Tested = c(35, 22, 18, 34, 31, 26, 14, 37, 17, 28, 26, 17),
+                     Proficient = c(14, 16, 10, 12, 13, 16, 9, 16, 3, 14, 17, 3),
+                     `Testing Rate` = c(0.833333333333333, 0.758620689655172, 0.857142857142857, 0.918918918918919, 0.794871794871795, 0.928571428571429, 0.666666666666667, 0.948717948717949, 0.607142857142857, 0.823529411764706, 0.742857142857143, 0.653846153846154),
+                     `Success Rate` = c(0.4, 0.727272727272727, 0.555555555555556, 0.352941176470588, 0.419354838709677, 0.615384615384615, 0.642857142857143, 0.432432432432432, 0.176470588235294, 0.5, 0.653846153846154, 0.176470588235294)
+                 ))
+})
